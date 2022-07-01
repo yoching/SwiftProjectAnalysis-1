6d015e03591c65cfbccbcdf4e25551f96255dea8
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2022-07-01 16:55:26 +0200

    Update to show table

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 use glob::*;
 use std::fs::File;
 use std::io::{self, BufRead};
+use tabled::{object::Columns, Alignment, Modify, Style, Table, Tabled};
 
 /// Simple project analyzation
 #[derive(Parser, Debug)]
@@ -9,6 +10,12 @@ struct Args {
     /// Path of root directory to analyze
     #[clap(short, long, value_parser, default_value = "./")]
     path: std::path::PathBuf,
+}
+
+#[derive(Tabled)]
+struct FileAnalysis {
+    name: String,
+    length: usize,
 }
 
 fn main() {
@@ -23,12 +30,30 @@ fn main() {
         .collect();
     let count = entries.len();
 
+    println!("## Summary");
     println!("{} files found", count);
+
+    let mut files: Vec<FileAnalysis> = Vec::new();
 
     for entry in entries {
         let file = File::open(entry.clone()).unwrap();
         let lines = io::BufReader::new(file).lines();
         let relative_path = entry.strip_prefix(&root_path).unwrap();
-        println!("- {}, {}", relative_path.display(), lines.count());
+        let analysis = FileAnalysis {
+            name: relative_path.to_string_lossy().to_string(),
+            length: lines.count(),
+        };
+        files.push(analysis);
     }
+
+    files.sort_by(|a, b| b.length.cmp(&a.length));
+
+    let table = Table::new(files)
+        .with(Style::psql())
+        .with(Modify::new(Columns::first()).with(Alignment::left()))
+        .with(Modify::new(Columns::last()).with(Alignment::right()))
+        .to_string();
+
+    println!("\n## Files");
+    println!("{}", table);
 }
