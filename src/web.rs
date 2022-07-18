8514c6d6428397da39972f46da6a0cc07b8f0d84
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2022-07-18 23:24:10 +0200

    Update to open browser after server start

--- src/web.rs
+++ src/web.rs
@@ -6,8 +6,16 @@ pub async fn start_http_server() -> std::io::Result<()> {
     // App::new().service(fs::Files::new("/*", "./web").index_file("index.html"))
     // App::new().service(spa().index_file("./web/index.html").finish())
 
-    HttpServer::new(|| App::new().service(fs::Files::new("/*", "./web").index_file("index.html")))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    let server = HttpServer::new(|| {
+        App::new().service(fs::Files::new("/*", "./web").index_file("index.html"))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run();
+
+    _ = tokio::join!(server, open_browser());
+    Ok(())
+}
+
+async fn open_browser() {
+    _ = open::that("http://127.0.0.1:8080/");
 }
