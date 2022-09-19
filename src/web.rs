fb8e8822d5b93da0b9115af8c1dd59bba45b5b6c
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2022-09-19 11:40:33 +0200

    Add Tokio time handling

--- src/web.rs
+++ src/web.rs
@@ -1,5 +1,7 @@
 use actix_files as fs;
 use actix_web::{App, HttpServer};
+use std::time::Duration;
+use tokio::time;
 
 #[actix_web::main]
 pub async fn start_http_server() -> std::io::Result<()> {
@@ -7,15 +9,22 @@ pub async fn start_http_server() -> std::io::Result<()> {
     // App::new().service(spa().index_file("./web/index.html").finish())
 
     let server = HttpServer::new(|| {
-        App::new().service(fs::Files::new("/*", "./web").index_file("index.html"))
+        let app = App::new().service(fs::Files::new("/*", "./web").index_file("index.html"));
+        // open_browser();
+        app
     })
     .bind(("127.0.0.1", 8080))?
     .run();
+
+    // server.await
 
     _ = tokio::join!(server, open_browser());
     Ok(())
 }
 
 async fn open_browser() {
-    _ = open::that("http://127.0.0.1:8080/");
+    tokio::time::sleep(Duration::from_secs(5)).await;
+    if let Err(err) = open::that("http://127.0.0.1:8080/") {
+        println!("faile to open browser: {}", err);
+    }
 }
