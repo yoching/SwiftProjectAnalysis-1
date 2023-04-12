a457d911b69bac9461ea5e4cfb3ebeb094b8b9d5
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2023-04-12 13:54:31 +0200

    Remove unnecessary console log

--- web/js/main.js
+++ web/js/main.js
@@ -2,8 +2,6 @@ import * as d3 from "https://cdn.skypack.dev/d3@7";
 
 import * as gridjs from "https://unpkg.com/gridjs/dist/gridjs.umd.js";
 
-console.log("Hello world!!");
-
 d3
     .json("file_stats.json")
     .then((data) => {
